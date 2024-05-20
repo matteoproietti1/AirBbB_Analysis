a2426e491be93729d5a735c51d5a34ace8449d18
Matteo Proietti <matteoproietti99@gmail.com>
2024-05-20 19:26:28 +0200

    Add files via upload
    R Script (ggplot for column chart)

---
+++ R Script/cheap_exp.R
@@ -0,0 +1,13 @@
+# Define color gradient from green to red
+color_palette <- colorRampPalette(c("green", "red"))
+
+# Bar chart comparing average prices
+ggplot(milan_combined, aes(x = reorder(neighbourhood, average_price), y = average_price, fill = average_price)) +
+  geom_bar(stat = "identity", position = "dodge") +
+  scale_fill_gradient(low = "green", high = "red") +
+  geom_text(aes(label = sprintf("$%.2f", average_price)), vjust = -0.5, size = 3.5, color = "black") +
+  labs(title = "Average Prices in Cheapest and Most Expensive Neighborhoods",
+       x = "Neighbourhood",
+       y = "Average Price") +
+  theme_minimal() +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1), plot.title = element_text(hjust = 0.5))
